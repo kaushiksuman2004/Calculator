fbdb77833ec2b3e0dac6f15c087fec3926f0d7ff
Kaushik <kaushiksuman94301@gmail.com>
2024-07-07 18:19:56 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,23 @@
+let inputBox = document.getElementById("inputBox");
+let buttons = document.getElementsByTagName("button");
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      inputBox.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      inputBox.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+    } else {
+      inputBox.value = string;
+      string += e.target.innerHTML;
+      inputBox.value = string;
+    }
+  });
+});
